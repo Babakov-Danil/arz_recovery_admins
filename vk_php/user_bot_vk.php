030c9afef502252c7ebbb6da042a5b9ad0ac2706
Hooziz <89225310+HOOIZIZ@users.noreply.github.com>
2022-06-20 14:08:42 +0400

    Update user_bot_vk.php

--- vk_php/user_bot_vk.php
+++ vk_php/user_bot_vk.php
@@ -5,7 +5,7 @@ use DigitalStar\vk_api\vk_api; // Основной класс
 use DigitalStar\vk_api\LongPoll; //работа с longpoll
 
 //token
-const VK_KEY = '75f310babf67213fd212d87a795133135cb7b35f1cb345d19433310bf6e8d34391c94801113762fa211f4'; //токен пользователя
+const VK_KEY = '***'; //токен пользователя
 const VERSION = '5.101'; //версия используемого api
 
 //authorization
