5e26b6760c54c5a6d149a8743f797e2a8a45547a
Danil Babakov <89225310+Babakov-Danil@users.noreply.github.com>
2022-11-16 19:55:54 +0300

    Update user_bot_vk.php

--- vk_php/user_bot_vk.php
+++ vk_php/user_bot_vk.php
@@ -13,8 +13,8 @@ $vk = vk_api::create(VK_KEY, VERSION);
 $vk = new LongPoll($vk);
 //
 $vk->listen(function($data)use($vk){ //longpoll для пользователя
-    if ($data[0] == 52 & $data[1] == 7 & $data[2] - 2000000000 == 232){
-        $chat_id = $data[2] - 2000000000;
-        $vk->kick($chat_id,$data[3]);
+    if ($data[0] == 52 & $data[1] == 7 & $data[2] - 2000000000 == 232){ //проверка id эвента, id диалога
+        $chat_id = $data[2] - 2000000000; // id диалога
+        $vk->kick($chat_id,$data[3]); // kick пользователя
     }
 });
